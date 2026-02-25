2d900b7dac559e737cb1b7d27fafcfdcaae019a9
Klawns <fabriciioklaus@gmail.com>
2026-02-25 13:50:10 -0300

    fix: fixed the publish-dockerhub and the memory database for spring tests

--- app/readium-backend/src/test/java/com/br/klaus/readium/BibliotecaVirtualApplicationTests.java
+++ app/readium-backend/src/test/java/com/br/klaus/readium/BibliotecaVirtualApplicationTests.java
@@ -2,8 +2,10 @@ package com.br.klaus.readium;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BibliotecaVirtualApplicationTests {
 
     @Test
